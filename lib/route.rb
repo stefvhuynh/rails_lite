ded733b16d83fd378c5b6f0bc0a0d7836688c19c
Stefan Huynh <huynh.stefan@gmail.com>
2014-09-09 10:50:30 -0400

    Finish router

--- lib/route.rb
+++ lib/route.rb
@@ -1,13 +1,23 @@
 class Route
-  attr_reader :pattern, :method, :controller, :action
-  
-  def initialize(pattern, method, controller, action)
-    @pattern, @method = pattern, method
-    @controller, @action = controller, action
+  attr_reader :pattern, :http_method, :controller_class, :action_name
+
+  def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method, = pattern, http_method
+    @controller_class, @action_name = controller_class, action_name
   end
-  
+
   def matches?(req)
-    req.request_method.downcase.to_sym == @method && pattern.match(req.path)
+    (req.request_method.downcase.to_sym == http_method) &&
+      !pattern.match(req.path).nil?
   end
-  
+
+  def run(req, res)
+    matched = pattern.match(req.path)
+
+    route_params = matched.names.each_with_object({}) do |name, route_params|
+      route_params[name] = matched[name]
+    end
+
+    controller_class.new(req, res, route_params).invoke_action(action_name)
+  end
 end
--- lib/router.rb
+++ lib/router.rb
@@ -1,6 +1,35 @@
 class Router
+  attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
+  def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
+  end
+
+  def draw(&proc)
+    instance_eval(&proc)
+  end
+
+  [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
+  end
+
+  def match(req)
+    @routes.find { |route| route.matches?(req) }
+  end
+
+  def run(req, res)
+    route = match(req)
+
+    if route.nil?
+      res.status = 404
+    else
+      route.run(req, res)
+    end
+  end
 end
